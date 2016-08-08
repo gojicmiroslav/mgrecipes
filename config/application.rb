56a770a2194cb7e3b6876f0b7fac098a7cf8d907
Miroslav Gojic <miroslavy2k@gmail.com>
2016-08-08 21:07:50 +0200

    Fix assets bug #1

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,19 @@ module Mgrecipes
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+    #config.assets.paths << Rails.root.join("bower_components")
+    #config.assets.paths << Rails.root.join("bower_components", "bootstrap-sass-official","assets","fonts")
+    #config.assets.paths << Rails.root.join("bower_components","bootstrap-sass-official","assets","stylesheets")
+    #config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
 
-    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
+    # Bower asset paths
+	root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+	  config.sass.load_paths << bower_path
+	  config.assets.paths << bower_path
+	end
+	# Precompile Bootstrap fonts
+	config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+	# Minimum Sass number precision required by bootstrap-sass
+	::Sass::Script::Number.precision = [8, ::Sass::Script::Number.precision].max    
   end
 end
